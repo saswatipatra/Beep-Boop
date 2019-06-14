49c387bbef110e35d1a575ad00e796c029119750
saswati <p403saswati@gmail.com>
2019-06-14 13:56:08 -0700

    debug errors

--- js/scripts.js
+++ js/scripts.js
@@ -13,16 +13,39 @@ return series;
 
 var printSeries= function(series, countNum){
   for (var i = 0; i <= countNum; i++) {
-    if (series[0]===3 || series[1]===3 || series[2]===3){
-      series.splice(i, 1, " I'm sorry Dave, I'm afraid I can't do that ");
-    }else if ((series[0]===2 || series[1]===2 || series[2]===2) && (series[0]!= 3 || series[1]!= 3 || series[2]!= 3)) {
-      series.splice(i, 1, " Boop! ");
-    }else if ((series[0]===1 || series[1]===1 || series[2]===1) && (series[0]!= 2 || series[1]!= 2 || series[2]!= 2) && (series[0]!= 3 || series[1]!= 3 || series[2]!= 3) ) {
-      series.splice(i, 1, " Beep! ");
+    var string;
+    var store=[];
+    string=series[i];
+    string=string.toString();
+    for (var j=0; j<=string; j++){
+      store=store.push(string.charAT(j));
+      if(store.includes('3')){
+        series[i].splice(i,1," I'm sorry Dave, I'm afraid I can't do that" );
+        return;
+      }else if (store.includes('2')) {
+        series[i].splice(i,1," Boop! " );
+        return;
+      }else if (stor.includes('1')) {
+        series[i].splice(i,1," Beep! " );
+        return;
+      }
     }
   }
- return series;
+  return series;
 }
+
+
+
+//     if (series[i]===3 || series[i]===3 || series[i]===3){
+//       series.splice(i, 1, " I'm sorry Dave, I'm afraid I can't do that ");
+//     }else if ((series[0]===2 || series[1]===2 || series[2]===2) && (series[0]!= 3 || series[1]!= 3 || series[2]!= 3)) {
+//       series.splice(i, 1, " Boop! ");
+//     }else if ((series[0]===1 || series[1]===1 || series[2]===1) && (series[0]!= 2 || series[1]!= 2 || series[2]!= 2) && (series[0]!= 3 || series[1]!= 3 || series[2]!= 3) ) {
+//       series.splice(i, 1, " Beep! ");
+//     }
+//   }
+//  return series;
+// }
 
 function mainPageButton()
 {
