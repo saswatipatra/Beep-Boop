10f53fc454faf745440d30d5c6c2490e8328a4f4
saswati <p403saswati@gmail.com>
2019-06-14 10:54:52 -0700

    add print series function in Js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,31 @@
+// Bussines logic
+var printSeries= function(countNum){
+  var series=[];
+  for (var i = 0; i <= countNum; i++) {
+  series.push(i);
+}
+return series;
+}
+
+function mainPageButton()
+{
+  location.reload();
+  
+}
+
+
+
+
+
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     var userInput= parseInt($("input#countNum").val());
-    
+    if (userInput != [^0-9]){
+      $(".retry").show();
+    }
+    var series= printSeries(userInput);
+    $(".series").text(series);
+    $(".result").show();
   });
 });
