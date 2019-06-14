1153d00755b9ed133013048dd749b03ba58b9eb6
saswati <p403saswati@gmail.com>
2019-06-14 11:22:21 -0700

    add function of Beep-boop valid for unit digit numbers.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,16 @@
 var printSeries= function(countNum){
   var series=[];
   for (var i = 0; i <= countNum; i++) {
-  series.push(i);
+    if (i===1){
+      series.push(" Beep! ");
+    }else if (i===2) {
+      series.push(" Boop! ");
+    }else if (i===3) {
+      series.push(" I'm sorry dave, I'm afraid i can't do that. ");
+    }else {
+      series.push(i);
+    }
+
 }
 return series;
 }
@@ -10,9 +19,15 @@ return series;
 function mainPageButton()
 {
   location.reload();
-  
+
 }
 
+function retryButton()
+{
+  $(".result").hide();
+  location.reload();
+
+}
 
 
 
@@ -21,11 +36,12 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     var userInput= parseInt($("input#countNum").val());
-    if (userInput != [^0-9]){
+    if (isNaN(userInput)){
       $(".retry").show();
-    }
+    }else{
     var series= printSeries(userInput);
     $(".series").text(series);
     $(".result").show();
+  }
   });
 });
